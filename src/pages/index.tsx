5a7cd466883091056da16d1cfbffa8fbe0d5ed3f
Lucas Diniz <lucasalvarengadiniz@gmail.com>
2021-04-21 20:35:27 -0300

    axios e formatações

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,20 @@
-// index.tsx
-// SSG
+import { GetStaticProps } from "next";
+import { format, parseISO } from "date-fns";
+import ptBR from 'date-fns/locale/pt-BR'
+import { api } from "../services/api";
 
-export default function Home(props) {
+type Episode = {
+  id: string,
+  title: string,
+  members: string,
+  published_at: string,
+}
+
+type HomeProps = {
+  episodes: Episode[];
+}
+
+export default function Home(props: HomeProps) {
   return (
     <div>
       <h1>Index</h1>
@@ -10,18 +23,33 @@ export default function Home(props) {
   );
 }
 
-export async function getStaticProps() {
-  const response = await fetch('http://localhost:3333/episodes');
-  const data = await response.json();
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  });
+
+  const episodes = data.map(episode => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      // Formatando os dados de data que estão vindo da chamada 
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', { locale: ptBR }),
+      duration: Number(episode.file.duration),
+      description: episode.description,
+      url: episode.file.url,
+    }
+  });
 
   return {
     props: {
       episodes: data,
     },
-
-    // Define o tempo em que uma nova versão da página é gerada.
-    // Recebe um valor em segundos.
-    // 8 horas definidas aqui.
     revalidate: 60 * 60 * 8,
   }
 }
