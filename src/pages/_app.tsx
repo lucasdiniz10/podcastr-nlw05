645e49ee10ef187a7233f20cc409a6961883b566
Lucas Diniz <lucasalvarengadiniz@gmail.com>
2021-04-24 03:26:51 -0300

    fim dia 4

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,14 +10,32 @@ import styles from '../styles/app.module.scss';
 function MyApp({ Component, pageProps }) {
   const [episodeList, setEpsodeList] = useState([]);
   const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
 
   function play(episode) {
     setEpsodeList([episode]);
     setCurrentEpisodeIndex(0);
+    setIsPlaying(true);
   };
 
+  function togglePlay() {
+    setIsPlaying(!isPlaying);
+  }
+
+  function setPlayingState(state: boolean) {
+    setIsPlaying(state);
+  }
+
   return (
-    <playerContext.Provider value={{ episodeList, currentEpisodeIndex, play }}>
+    <playerContext.Provider
+      value={{
+        episodeList,
+        currentEpisodeIndex,
+        play,
+        isPlaying,
+        togglePlay,
+        setPlayingState,
+      }}>
       <div className={styles.wrapper}>
         <main>
           <Header />
